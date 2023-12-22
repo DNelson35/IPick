f40221c19a4cae809a131e468cb5c9a8b070f206
Damien Nelson <dhnisaman@gmail.com>
2023-12-21 21:07:29 -0500

    import wheel comp for testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
-"use client"
 import React, { useState } from "react";
 import SpinningWheel from "@/components/SpinningWheel";
+import Wheel from "@/components/Wheel";
 
 const Home: React.FC = () => {
- const [stopSpin, setStopSpin] = useState<boolean>(false);
+//  const [stopSpin, setStopSpin] = useState<boolean>(false);
 
- const handleSpin = () => {
-   setStopSpin(prevStopSpin => !prevStopSpin);
- };
+//  const handleSpin = () => {
+//    setStopSpin(prevStopSpin => !prevStopSpin);
+//  };
 
  return (
    <main className="flex justify-center items-center h-screen w-full">
-     <SpinningWheel stopSpin={stopSpin} />
-     <button onClick={handleSpin}>Spin</button>
+      <Wheel />
+     {/* <SpinningWheel stopSpin={stopSpin} />
+     <button onClick={handleSpin}>Spin</button> */}
    </main>
  );
 };
