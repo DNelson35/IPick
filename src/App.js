3d77e9fc4d912ed485e0ee3ec03862e01a5caa17
Damien Nelson <dhnisaman@gmail.com>
2024-02-10 11:10:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import WheelComponent from "react-wheel-of-prizes";
 
 function App() {
+  const segments = ["Mc Donalds", "Burger King", "Arbys", "KFC", "Pizza Hut"];
+  const segColors = ["#EE4040", "#F0CF50", "#815CD1", "#3DA5E0", "#34A24F"];
+  const onFinished = (winner) => {
+    console.log(winner);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <WheelComponent
+          segments={segments}
+          segColors={segColors}
+          onFinished={(winner) => onFinished(winner)}
+          primaryColor="black"
+          contrastColor="white"
+          buttonText="Spin"
+          isOnlyOnce={false}
+          size={190}
+          upDuration={50}
+          downDuration={600}
+          fontFamily="Arial"
+        />
+      </div>
     </div>
   );
 }
